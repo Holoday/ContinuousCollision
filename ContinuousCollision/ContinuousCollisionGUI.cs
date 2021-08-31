fd8b66ceab025ddf834fa11008ec52b509df4f17
Lawrence <30965946+LawrenceJN@users.noreply.github.com>
2021-08-31 01:29:30 +0100

    Added an automatic system for applying continuous collision and reworked the GUI. (#1)
    * Added an automatic system for applying continuous collision and reworked the GUI.
    
    * Removed an empty line.
    
    * Shrank a couple of if statements.

--- ContinuousCollision/ContinuousCollisionGUI.cs
+++ ContinuousCollision/ContinuousCollisionGUI.cs
@@ -1,11 +1,14 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using KSP.UI.Screens;
+using System.Linq;
 
 internal class ContinuousCollisionGUI : MonoBehaviour
 {
     // Internal GUI logic variables.
-    private Rect windowRect = new Rect(230, 10, 500, 500);
+    private Rect windowRect = new Rect(100, 100, 0, 0);
+    private Vector2 scrollPositionAllVessels;
+    private bool showAutoStats = false;
     // Used for the toggle switches, as to only allow one to be enabled at once,
     // defaulting to Discrete Collision.
     private bool[] modeToggleBools = new bool[] { false, false, false, true, false };
@@ -18,6 +21,7 @@ internal class ContinuousCollisionGUI : MonoBehaviour
     // Public read only variables
     public CollisionDetectionMode DesiredCollisionMode { get { return desiredCollisionMode; } }
     public CollisionDetectionMode CurrentCollisionMode = CollisionDetectionMode.Discrete;
+    public bool desireAuto = true;
     public bool ApplyAtOnce;
     public bool ApplyAsync;
 
@@ -29,44 +33,72 @@ internal class ContinuousCollisionGUI : MonoBehaviour
     public static bool guiEnabled = false;
 
     public void DrawGUI() =>
-        windowRect = GUI.Window(0, windowRect, drawWindow, "Continuous Collision GUI");
+        windowRect = GUILayout.Window(0, windowRect, FillWindow, "Continuous Collision", GUILayout.Height(1), GUILayout.Width(350));
     public void StartCoroutines() =>
-        StartCoroutine(setFPS(0.5f)); 
+        StartCoroutine(setFPS(0.5f));
 
-    private void drawWindow(int windowID)
+    private void FillWindow(int windowID)
     {
-        GUI.Label(new Rect(20, 30, 460, 40),
-            "This is an early release, not meant for stable use. " +
+        GUIStyle boxStyle = GUI.skin.GetStyle("Box");
+
+        GUILayout.BeginVertical(boxStyle);
+        GUILayout.Label("This is an early release, not meant for stable use. " +
             "We are not responsible if your game crashes or otherwise breaks.");
+        GUILayout.EndVertical();
 
-        GUI.Label(new Rect(20, 100, 460, 20), "Select Collision Mode");
+        desireAuto = GUILayout.Toggle(desireAuto, "Auto");
 
-        newModeToggleBools[0] = GUI.Toggle(new Rect(20, 120, 460, 20), modeToggleBools[0], " Continuous-Dynamic (best, least performance.)");
-        newModeToggleBools[1] = GUI.Toggle(new Rect(20, 140, 460, 20), modeToggleBools[1], " Continuous");
-        newModeToggleBools[2] = GUI.Toggle(new Rect(20, 160, 460, 20), modeToggleBools[2], " Continuous-Speculative");
-        newModeToggleBools[3] = GUI.Toggle(new Rect(20, 180, 460, 20), modeToggleBools[3], " Discrete (defualt, worst, most performance.)");
-        if (!isApplyingAsync)
+        if (!desireAuto)
         {
-            VerifyToggles();
-            AssertainDesiredCollisionMode();
-            isApplied = DesiredCollisionMode == CurrentCollisionMode;
+            GUILayout.Label("Select Collision Mode");
+
+            newModeToggleBools[0] = GUILayout.Toggle(modeToggleBools[0], " Continuous-Continuous (best, least performance.)");
+            newModeToggleBools[1] = GUILayout.Toggle(modeToggleBools[1], " Continuous");
+            newModeToggleBools[2] = GUILayout.Toggle(modeToggleBools[2], " Continuous-Speculative");
+            newModeToggleBools[3] = GUILayout.Toggle(modeToggleBools[3], " Discrete (defualt, worst, most performance.)");
+            if (!isApplyingAsync)
+            {
+                VerifyToggles();
+                AssertainDesiredCollisionMode();
+                isApplied = DesiredCollisionMode == CurrentCollisionMode;
+            }
+
+            GUILayout.Label(isApplied ? $"All changes applied. Number of rigidbodies updated: {rigidbodyCount.ToString()}." : "Not Applied Yet!");
+
+            if (GUILayout.Button("Apply") && !isApplyingAsync)
+            {
+                ApplyAtOnce = true;
+                ApplyAsync = false;
+            }
+            if (GUILayout.Button(isApplyingAsync ? "Applying Async..." : "Apply Async") && !isApplyingAsync)
+            {
+                ApplyAtOnce = false;
+                ApplyAsync = true;
+            }
+        }
+        else
+        {
+            if (showAutoStats = GUILayout.Toggle(showAutoStats, "Show Stats"))
+            {
+                GUILayout.BeginVertical(boxStyle);
+                scrollPositionAllVessels = GUILayout.BeginScrollView(scrollPositionAllVessels, GUILayout.Height(300));
+
+                foreach (Vessel vessel in ContinuousCollision.loadedVessels)
+                {
+                    GUILayout.BeginVertical(boxStyle);
+                        GUILayout.Label(vessel.GetDisplayName());
+                        int continuousParts = vessel.Parts.Count(p => p.Rigidbody.collisionDetectionMode == CollisionDetectionMode.ContinuousDynamic);
+                        GUILayout.Label($"Continuous Rigidbodies: {continuousParts.ToString()}");
+                        GUILayout.Label($"Unpacked: {!vessel.packed}");
+                    GUILayout.EndVertical();
+                }
+
+                GUILayout.EndScrollView();
+                GUILayout.EndVertical();
+            }
         }
 
-        GUI.Toggle(new Rect(20, 220, 460, 20), false, " Auto (Not configued.)");
-        GUI.Label(new Rect(20, 250, 460, 20), (isApplied ? $"All changes applied. Number of rigidbodies updated: {rigidbodyCount.ToString()}." : "Not Applied Yet!"));
-
-        if (GUI.Button(new Rect(20, 280, 110, 20), "Apply") && !isApplyingAsync)
-        {
-            ApplyAtOnce = true;
-            ApplyAsync = false;
-        }
-        if (GUI.Button(new Rect(140, 280, 110, 20), isApplyingAsync ? "Applying Async..." : "Apply Async") && !isApplyingAsync)
-        {
-            ApplyAtOnce = false;
-            ApplyAsync = true;
-        }
-
-        GUI.Label(new Rect(20, 470, 150, 20), $"Current FPS: {framesPerSecond.ToString()}");
+        GUILayout.Label($"Current FPS: {framesPerSecond.ToString()}");
 
         GUI.DragWindow(new Rect(0, 0, 10000, 500));
     }
@@ -111,15 +143,6 @@ internal class ContinuousCollisionGUI : MonoBehaviour
             desiredCollisionMode = CollisionDetectionMode.Discrete;
             return;
         }
-        if (modeToggleBools[4])
-        {
-            // reset, if this is somehow triggered, as auto is not configured yet.
-            Debug.LogWarning("modeToggleBools[4] (auto) selected erroneously! resetting...");
-            modeToggleBools[4] = false;
-            modeToggleBools[3] = true;
-            return;
-        }
-
     }
 
     public IEnumerator setFPS(float refreshRate)
@@ -147,7 +170,7 @@ internal class ContinuousCollisionGUI : MonoBehaviour
         if (!addedAppLauncherButton)
         {
             Texture buttonTexture = GameDatabase.Instance.GetTexture("ContinuousCollision/Textures/icon", false);
-            ApplicationLauncher.Instance.AddModApplication(ToggleGui, ToggleGui, null, null, null, null, (ApplicationLauncher.AppScenes)63, buttonTexture);
+            ApplicationLauncher.Instance.AddModApplication(ToggleGui, ToggleGui, null, null, null, null, ApplicationLauncher.AppScenes.FLIGHT, buttonTexture);
             addedAppLauncherButton = true;
         }
     }
